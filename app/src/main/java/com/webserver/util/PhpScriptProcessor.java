a6410ea6d51838a9015ca3f15d30a4669515842a
Ana45639 <aodonne8@tcd.ie>
2025-03-30 16:41:03 +0100

    added API key

--- app/src/main/java/com/webserver/util/PhpScriptProcessor.java
+++ app/src/main/java/com/webserver/util/PhpScriptProcessor.java
@@ -7,8 +7,8 @@ import java.io.OutputStream;
 
 public class PhpScriptProcessor extends ScriptProcessor {
     //Add the url for the flask server on the VM below
-    private static final String API_URL = "";
-
+    private static final String API_URL = "http://localhost:5000/run-php";
+    private static final String API_KEY = "sk-L93v8XrAqY6uTdNz54fGwPUcRb7yCxMTqE2JhK1L";
     @Override
     public String processScript(String filepath) {
         try {
@@ -21,9 +21,12 @@ public class PhpScriptProcessor extends ScriptProcessor {
 
             //Create a URL connection to connect to the API
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestProperty("X-API-Key", API_KEY);
             conn.setRequestMethod("POST");
             conn.setRequestProperty("Content-Type", "application/json; utf-8");
             conn.setDoOutput(true);
+
+
 
             // Send the request body by writing the JSON payload
             try (OutputStream os = conn.getOutputStream()) {
