ee39d71e5a3d773d924e2be9bd428b9ec7b6a431
Ana45639 <aodonne8@tcd.ie>
2025-03-30 19:20:11 +0100

    move API_KEY and URL to be stored in the .env file

--- app/src/main/java/com/webserver/util/PhpScriptProcessor.java
+++ app/src/main/java/com/webserver/util/PhpScriptProcessor.java
@@ -1,4 +1,6 @@
 package com.webserver.util;
+import io.github.cdimascio.dotenv.Dotenv;
+
 import java.net.HttpURLConnection;
 import java.nio.charset.StandardCharsets;
 import java.net.URL;
@@ -7,8 +9,11 @@ import java.io.OutputStream;
 
 public class PhpScriptProcessor extends ScriptProcessor {
     //Add the url for the flask server on the VM below
-    private static final String API_URL = "http://localhost:5000/run-php";
-    private static final String API_KEY = "sk-L93v8XrAqY6uTdNz54fGwPUcRb7yCxMTqE2JhK1L";
+    private static final Dotenv dotenv = Dotenv.load();
+    private static final String API_KEY = dotenv.get("API_KEY");
+    private static final String API_URL = dotenv.get("API_URL"); //for local testing "http://localhost:5000/run-php";
+
+
     @Override
     public String processScript(String filepath) {
         try {
@@ -21,14 +26,14 @@ public class PhpScriptProcessor extends ScriptProcessor {
 
             //Create a URL connection to connect to the API
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            //Load the API KEY
             conn.setRequestProperty("X-API-Key", API_KEY);
             conn.setRequestMethod("POST");
             conn.setRequestProperty("Content-Type", "application/json; utf-8");
             conn.setDoOutput(true);
 
-
-
-            // Send the request body by writing the JSON payload
+            // Send the request body by writing the JSON
             try (OutputStream os = conn.getOutputStream()) {
                 byte[] input = jsonInput.getBytes(StandardCharsets.UTF_8);
                 os.write(input, 0, input.length);
