510209e0fa44e6d621421a8f3cd41a3e723682d3
Ana45639 <aodonne8@tcd.ie>
2025-04-04 16:17:21 +0100

    added new test app

---
+++ app/test_scripting/app.php
@@ -0,0 +1,16 @@
+<?php
+$name = "Cloudle User";
+echo "<h2>Hello, $name!</h2>";
+echo "<ul>";
+for ($i = 1; $i <= 3; $i++) {
+    echo "<li>App #$i deployed!</li>";
+}
+echo "</ul>";
+?>
+
+<?php
+// Output some HTML content
+echo "<h1>Hello from PHP!</h1>";
+echo "<p>The current server time is: " . date("Y-m-d H:i:s") . "</p>";
+?>
+
