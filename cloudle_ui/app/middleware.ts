04bc98a7ce7284aa299b6aaa258a68a7811e47c8
Yundy <yua1@tcd.ie>
2025-04-04 01:51:12 +0100

    fixed middleware

--- cloudle_ui/app/middleware.ts
+++ cloudle_ui/app/middleware.ts
@@ -1,6 +1,6 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(req) {
+export function middleware(req: NextRequest) {
   const token = req.cookies.get("token");
 
   if (!token && req.nextUrl.pathname !== "/login" && req.nextUrl.pathname !== "/register_account") {
